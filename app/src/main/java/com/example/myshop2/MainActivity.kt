a65f65601326f4abe7ea0b25f1806a2ec760c924
praktikan <praktikan@SV-EIHU112-013.local>
2025-09-15 15:25:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myshop2/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myshop2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.myshop2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    // buat binding
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        with(receiver = binding) {
+
+            val navController = findNavController(R.id.nav_host_fragment)
+
+            bottomNavigationView.setupWithNavController(navController)
+        }
+    }
+}
